cec9c0880059d186a950d420fdb4f9967f87eaaf
sumandas473 <dass52120@gmail.com>
2025-07-17 20:33:25 +0530

    fix3

--- src/app/page.js
+++ src/app/page.js
@@ -7,13 +7,14 @@ import { Cover } from "@/components/ui/cover";
 import { Skills } from "@/components/Skills";
 import { ShootingStars } from "@/components/ui/shooting-stars";
 import { StarsBackground } from "@/components/ui/stars-background";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="w-full overflow-x-hidden min-h-screen grid lg:grid-cols-[1fr_0.6fr_0.4fr] gap-[20px]">
 
       <div className="max-w-[600px] w-[90%] mx-auto py-8">
-        <a href="/" className="font-bold text-amber-50"> <Image src={logo} width={120} height={80} className="max-h-[80px] h-full object-contain object-center" alt="logo" /></a>
+        <Link href="/" className="font-bold text-amber-50"> <Image src={logo} width={120} height={80} className="max-h-[80px] h-full object-contain object-center" alt="logo" /></Link>
         {/* left side content  */}
         <div className="flex flex-col justify-center h-[80%]">
           <span className="text-blue-400 text-sm md:text-lg ">Its me</span>
@@ -21,9 +22,9 @@ export default function Home() {
             Suman Das, <br /> Web & App <Cover> Developer</Cover>
           </h2>
 
-          <a href="#" className=" text-white  border-[#525252] border-b hover:p-[10px] hover:bg-[#5070ff2f] mr-auto text-start ease-in-out duration-200 py-2 hover:px-5 hover:rounded-full cursor-pointer">
+          <Link href="#" className=" text-white  border-[#525252] border-b hover:p-[10px] hover:bg-[#5070ff2f] mr-auto text-start ease-in-out duration-200 py-2 hover:px-5 hover:rounded-full cursor-pointer">
             Contact Me &rarr;
-          </a>
+          </Link>
         </div>
 
       </div>
@@ -36,7 +37,7 @@ export default function Home() {
       {/* right side content */}
 
       <div className="w-[90%] mx-auto py-10 flex flex-col items-center z-3">
-        <a href="#" className="max-w-32 w-full h-[35px] flex justify-center items-center  border-1 border-[#525252] mx-auto pb-1 rounded-4xl">Hire me</a>
+        <Link href="#" className="max-w-32 w-full h-[35px] flex justify-center items-center  border-1 border-[#525252] mx-auto pb-1 rounded-4xl">Hire me</Link>
 
 
         <h2 className=" my-12 md:my-1  bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-2xl md:text-4xl lg:text-5xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
@@ -44,9 +45,9 @@ export default function Home() {
         </h2>
         <p className="max-w-xl mx-auto text-sm md:text-md text-neutral-700 dark:text-neutral-400 text-start ">
           I specialize in modern frontend development with a strong focus on creating clean, responsive, and engaging user interfaces. My core expertise lies in React.js, where I build scalable component-based applications using tools like Vite for fast development and optimized builds.
-          I'm proficient in JavaScript (ES6+), with solid knowledge of HTML5 and CSS3, and I often use Tailwind CSS to rapidly create modern, utility-first UI designs. For animations and motion effects, I rely on Framer Motion to bring interfaces to life with smooth transitions.
+          I&aposm proficient in JavaScript (ES6+), with solid knowledge of HTML5 and CSS3, and I often use Tailwind CSS to rapidly create modern, utility-first UI designs. For animations and motion effects, I rely on Framer Motion to bring interfaces to life with smooth transitions.
           I handle client-side routing using React Router DOM and manage application state with React Context API. I follow mobile-first and accessible design principles to ensure performance and usability across all devices.
-          In terms of tooling, I'm experienced with Git and GitHub for version control, and I use VS Code as my primary development environment. I'm also familiar with npm and pnpm for managing project dependencies.
+          In terms of tooling, I&aposm experienced with Git and GitHub for version control, and I use VS Code as my primary development environment. I&aposm also familiar with npm and pnpm for managing project dependencies.
       
         </p>
 
